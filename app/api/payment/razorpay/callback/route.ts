6c19c2772d936bd22d975d1355e7775fcf67b0de
ishu17077 <ishu17077@users.noreply.github.com>
2026-08-21 00:29:53 +0530

    Emergency patch: data['payload'] instead of sending data to controller

--- app/api/payment/razorpay/callback/route.ts
+++ app/api/payment/razorpay/callback/route.ts
@@ -14,6 +14,6 @@ export async function POST(req: NextRequest) {
         return sendResponse({ success: false, errors: "Invalid Signature", statusCode: 400 })
     }
 
-    await updatePaymentAndUpdateTicket(data.event, data)
+    await updatePaymentAndUpdateTicket(data.event, data["payload"])
     return sendResponse({ success: true, statusCode: 200 })
 }
